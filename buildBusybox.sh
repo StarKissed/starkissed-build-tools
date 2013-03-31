6501e209115702949fee240e36f3176d86ac3800
TwistedUmbrella <TwistedUmbrella@gmail.com>
2013-03-31 19:43:45 -0400

    Import modified NDK platform headers for portability of busybox compile

--- buildBusybox.sh
+++ buildBusybox.sh
@@ -7,14 +7,22 @@
 if cat /etc/issue | grep Ubuntu; then
     BUSYBSPEC=~/android/busybox-android
     TOOLCHAIN_PREFIX=~/android/android-toolchain-eabi/bin/arm-eabi-
+    FUSEREPO=~/Dropbox/TwistedServer/ScriptFusion/binaryfiles
+    SEDOUTPT='s/CONFIG_SYSROOT=.*$/CONFIG_SYSROOT=\"~\/android\/android-ndk-14\/arch-arm\"/'
 else
     BUSYBSPEC=/Volumes/android/busybox-android
     TOOLCHAIN_PREFIX=/Users/TwistedZero/Applications/android-ndk/toolchains/arm-linux-androideabi-4.7/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-
+    FUSEREPO=/Users/TwistedZero/Public/Dropbox/TwistedServer/ScriptFusion/binaryfiles
+    STARREPO=/Users/TwistedZero/Dropbox/StarKissedMod/res/raw
+    DUALREPO=/Users/TwistedZero/Dropbox/NexusDualBoot/res/raw
+    SEDOUTPT='s/CONFIG_SYSROOT=.*$/CONFIG_SYSROOT=\"\/Volumes\/android\/android-ndk-14\/arch-arm\"/'
 fi
 BSYCONFIG=loungekatt_config
 CPU_JOB_NUM=8
 
 cd $BUSYBSPEC
+
+sed -i $SEDOUTPT configs/$BSYCONFIG
 
 cp -R configs/$BSYCONFIG .config
 
@@ -27,3 +35,15 @@ make clean -j$CPU_JOB_NUM
 make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN_PREFIX
 
 file busybox
+
+if [ -e busybox ]; then
+    cp -R busybox $FUSEREPO/busybox
+    if [ $STARREPO ]; then
+        cp -R busybox $STARREPO/busybox
+    fi
+    if [ $DUALREPO ]; then
+        cp -R busybox $DUALREPO/busybox
+    fi
+fi
+
+
